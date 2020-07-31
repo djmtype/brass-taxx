0fe8d3c9c9d46dc0feceae4e7ecdc4d7123dc651
Sanity.io <hello@sanity.io>
2020-07-31 15:12:07 -0400

    Initial commit

---
+++ web/README.md
@@ -0,0 +1,1 @@
+# brass-taxx-web
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID || 'or6pfi79',
+    dataset: process.env.SANITY_DATASET || 'production'
+  }
+}
